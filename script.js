bbd234db4910d27e592fe36f384e01d65c6f11bd
81070111Nik <98652429+81070111Nik@users.noreply.github.com>
2022-04-11 22:34:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let weight = document.querySelector('#weight'),
+vol = document.querySelector('#vol'),
+load = document.querySelector('#load'),
+del = document.querySelector('#del'),
+frag = document.querySelector('#frag'),
+dist = document.querySelector('.distance').textContent = 500,
+range = document.querySelector('#range'),
+btn = document.querySelector('#btn'),
+out = document.querySelector('#out');
+
+range.onchange = function(){
+    document.querySelector('.distance').textContent = range.value;
+}
+povtor = function(){
+    if(load.checked){
+        load.value = 300;
+    }else{
+        load.value = 0;
+    }
+    if(del.checked){
+        del.value = 100;
+    }else{
+        del.value = 0;
+    }
+    if(frag.checked){
+        frag.value = 500;
+    }else{
+        frag.value = 0;
+    }
+    let sum = Number(range.value*2) + Number(weight.value*3) + Number(vol.value*4)+ Number(load.value)+ Number(del.value)+ Number(frag.value);
+    out.textContent = sum;
+}
+btn.onclick = function(){
+    if(weight.value ==="" && vol.value !=""){
+       alert('Укажите вес');
+    }else  if(weight.value !="" && vol.value ===""){
+        alert('Укажите объем');
+    }else if(weight.value !="" && vol.value !=""){
+        povtor();
+    }else{
+        alert('Укажите вес и объем');
+    }
+   
+    }
+    
